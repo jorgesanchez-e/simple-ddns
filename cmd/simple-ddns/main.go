8004ec654d21271a692fe4924b78f6d4ba5b0f3f
Jorge sanchez <jorge.sanchez.escobedo@gmail.com>
2025-04-22 14:57:32 -0600

    Added simple logger (#5)

--- cmd/simple-ddns/main.go
+++ cmd/simple-ddns/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/jorgesanchez-e/simple-ddns/internal/config"
+	"github.com/jorgesanchez-e/simple-ddns/internal/log"
 )
 
 func main() {
+	log := log.New(log.Error)
 	config, err := config.New()
 	if err != nil {
 		log.Fatal(err)
